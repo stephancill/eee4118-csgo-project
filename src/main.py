1b3a98cbd8377999e3e2d91af001a2f58d75fa2e
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-05-16 21:58:30 +0200

    Write recording to csv

--- src/main.py
+++ src/main.py
@@ -5,8 +5,10 @@ import time
 from gsi import GSIServer
 from collections import namedtuple
 import mouse
+import os
 
 ANG_ADDR = 0x9FD2808
+RECORDING_DIRECTORY = "recordings"
 
 Sample = namedtuple("Sample", ["x", "y", "clip", "time"])
 samples = []
@@ -17,6 +19,8 @@ class Recorder:
         self.start_time = None
     
     def start(self):
+        if not os.path.exists(RECORDING_DIRECTORY):
+            os.makedirs(RECORDING_DIRECTORY)
         self.start_time = time.time()
         self.samples = []
         print("started", self.start_time, bool(self.start_time))
@@ -24,9 +28,14 @@ class Recorder:
     def sample(self, x, y, clip):
         self.samples.append(Sample(x, y, clip, time.time() - self.start_time))
 
-    def end(self):
+    def end(self, write=False):
         print("ended", self.start_time)
         print("ended", len(self.samples), "samples", time.time() - self.start_time, "seconds")
+        if write:
+            with open(f"{RECORDING_DIRECTORY}/spray_{str(round(self.start_time))}.csv", "w") as f:
+                f.write("x,y,clip,time\n")
+                for sample in self.samples:
+                    f.write(",".join([str(x) for x in [sample.x, sample.y, sample.clip, sample.time]]) + "\n")
         self.start_time = None
 
     @property
@@ -73,7 +82,7 @@ def main():
             recorder.sample(x_angle, y_angle, ammo)
 
         if ammo == 0 and recorder.is_recording:
-            recorder.end()
+            recorder.end(write=True)
 
         time.sleep(0.01)
 
