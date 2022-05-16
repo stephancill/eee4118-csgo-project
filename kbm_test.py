6be6fd02dc077e5892bbb1fe214e2d947429f260
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-05-16 15:24:24 +0200

    Test

--- kbm_test.py
+++
@@ -1,6 +0,0 @@
-import mouse 
-import time
-
-for i in range(10):
-    mouse.move(50,50)
-    time.sleep(1)
--- src/kbm_test.py
+++ src/kbm_test.py
@@ -1,4 +1,9 @@
-import mouse
-import keyboard
+import mouse 
+import time
 
-mouse.move(50, 50, absolute=False, duration=2, steps_per_second=120.0)
+if __name__ == "__main__":
+    mouse.move(50,50, absolute=True)
+
+    for i in range(10):
+        mouse.move(50,50, absolute=False)
+        time.sleep(0.01)
