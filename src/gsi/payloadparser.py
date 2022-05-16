3e93344c52efa8c4a67dc6d72d8559f7be92795a
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-05-16 18:00:48 +0200

    WIP gsi

---
+++ src/gsi/payloadparser.py
@@ -0,0 +1,11 @@
+import gamestate
+
+class PayloadParser:
+    def parse_payload(self, payload, gamestate):
+        gamestate.payload = payload
+        for item in payload:
+            for i in payload[item]:
+                try:
+                    setattr(getattr(gamestate, item), i, payload[item][i])
+                except:
+                    pass
