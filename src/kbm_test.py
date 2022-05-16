18678ebfa0a3e157e9ff7ecbe8d01524d468e02b
sinsin1324 <84076434+sinsin1324@users.noreply.github.com>
2022-05-16 20:12:29 +0200

    kbm_test now has toggle

--- src/kbm_test.py
+++ src/kbm_test.py
@@ -1,9 +1,23 @@
+from turtle import onscreenclick
 import mouse 
 import time
+import keyboard
+
+on_off_switch = False
+
+def toggle():
+    global on_off_switch
+    on_off_switch = not on_off_switch
 
 if __name__ == "__main__":
-    mouse.move(50,50, absolute=True)
+    keyboard.add_hotkey('/', lambda: toggle())
+    mouse.move(25,25, absolute=False, duration=1)
+    time.sleep(1)
 
-    for i in range(10):
-        mouse.move(50,50, absolute=False)
-        time.sleep(0.01)
+    while(True):
+        if (on_off_switch):
+            mouse.move(-50,0, absolute=False, duration=1)
+            mouse.move(0,-50, absolute=False, duration=1)
+            mouse.move(50,0, absolute=False, duration=1)
+            mouse.move(0,50, absolute=False, duration=1)
+            time.sleep(0.01)
